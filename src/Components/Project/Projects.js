440762a85b38899cab711fa2930ece1a7d3e72d2
adilkhan <adilkhan@ucsb.edu>
2020-02-09 02:45:47 -0800

    Commit : added some more tweaks to the portfolio

--- src/Components/Project/Projects.js
+++ src/Components/Project/Projects.js
@@ -6,7 +6,7 @@ export default class Projects extends Component {
     render() {
         const { projects } = this.props
         return (
-            <div>
+            <div className="projects-list">
                 {projects.map((p)=>{
                     return(<ProjectCard skills={p.skills} title={p.title} description={p.description} img={p.image} links={p.links} />)
                 })}
