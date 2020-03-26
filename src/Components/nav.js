c0a5cbbb0c41429b55ac9d6bb8243c3fa7084cc2
adilkhan <adilkhan@ucsb.edu>
2020-03-26 16:13:47 -0700

    Replaced the logo and fixed styling

--- src/Components/nav.js
+++ src/Components/nav.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import {NavLink} from 'react-router-dom'
+import logo from '../Images/logo.png'
 
 class Nav extends Component{
     render(){
         return(
-            <nav className="nav">                
+            <nav className="nav">
+                <img src ={logo} style={{width : "90px", height : "60px", position: "fixed", left: "0", top : "0"}}/>                
                 <NavLink className="nav-item" activeClassName="active-link" exact to='/'>Home</NavLink>
                 <NavLink className="nav-item" exact activeClassName='active-link' to='/projects'>Projects</NavLink>
             </nav>
